876e575dbaaf187738eb368d77514a46ec69ebcd
Drew Rowan <andrew.rowan@wallee.com>
2021-10-18 12:26:10 +0200

    Release 3.0.8

--- README.md
+++ README.md
@@ -32,13 +32,13 @@ NOTE: RestSharp versions greater than 106.2.1 have a bug which causes file uploa
 ## Installation
 ```
 # Package Manager
-Install-Package PostFinanceCheckout -Version 3.0.7
+Install-Package PostFinanceCheckout -Version 3.0.8
 # .NET CLI
-dotnet add package PostFinanceCheckout --version 3.0.7
+dotnet add package PostFinanceCheckout --version 3.0.8
 # Paket CLI
-paket add PostFinanceCheckout --version 3.0.7
+paket add PostFinanceCheckout --version 3.0.8
 # PackageReference
-<PackageReference Include="PostFinanceCheckout" Version="3.0.7" />
+<PackageReference Include="PostFinanceCheckout" Version="3.0.8" />
 ```
 
 Then include the DLL (under the `bin` folder) in the C# project, and use the namespaces:
--- src/PostFinanceCheckout/Client/Configuration.cs
+++ src/PostFinanceCheckout/Client/Configuration.cs
@@ -19,7 +19,7 @@ namespace PostFinanceCheckout.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "3.0.7";
+        public const string Version = "3.0.8";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -90,8 +90,8 @@ namespace PostFinanceCheckout.Client
             }
             _authenticationKey =  authenticationKey;
             _applicationUserID = applicationUserID;
-            UserAgent = "PostFinanceCheckout/3.0.7/csharp";
-            BasePath = "https://app-wallee.com:443";
+            UserAgent = "PostFinanceCheckout/3.0.8/csharp";
+            BasePath = "https://checkout.postfinance.ch:443/api";
             DefaultHeader = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
             ApiKeyPrefix = new ConcurrentDictionary<string, string>();
@@ -338,8 +338,8 @@ namespace PostFinanceCheckout.Client
             String report = "C# SDK (PostFinanceCheckout) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 3.0.7\n";
-            report += "    SDK Package Version: 3.0.7\n";
+            report += "    Version of the API: 3.0.8\n";
+            report += "    SDK Package Version: 3.0.8\n";
 
             return report;
         }
